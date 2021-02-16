a6ca85ffe78cb17953c00d0f1c823e3f9ca40427
yamada-inselart <g-account@insel-art.jp>
2021-02-16 12:19:55 +0900

    簡単検索 見出し調整

--- index.php
+++ index.php
@@ -280,6 +280,7 @@ require_once($Root . "/office_search/html_lib.php");
 		.search_cont .SearchBy_cont .SearchBy_inputWrap {
 			display: inline-block;
 		}
+
 		.search_cont .SearchBy_cont .SearchBy_buttonWrap {
 			display: inline-block;
 		}
@@ -317,6 +318,7 @@ require_once($Root . "/office_search/html_lib.php");
 		.SearchSect button:focus {
 			outline: none;
 		}
+
 		.SearchSect .SearchSect_wrap .SearchBtn {
 			background: none;
 			border: none;
@@ -338,6 +340,7 @@ require_once($Root . "/office_search/html_lib.php");
 		.SearchSect.SearchSect-theme .SearchSect_wrap {
 			border: 1px solid #f46991;
 		}
+
 		.SearchSect .search_subttl.search_subttl-03 {
 			background: #f46991;
 		}
@@ -348,25 +351,29 @@ require_once($Root . "/office_search/html_lib.php");
 
 		/* SP */
 		@media screen and (max-width:767px) {
+
 			/* セクション全体 */
 			.SearchSect_wrap {
 				padding: 5px;
 			}
+
 			.SearchSect_wrap .search_cont {
 				font-size: 14px;
 				padding: 10px;
 			}
+
 			.SearchSect_wrap .search_cont .SearchBy_cont {
 				box-sizing: border-box;
 				display: block;
 				padding: 15px;
 				width: 100%;
 			}
-			
+
 			/* 見出し */
 			.SearchSect_wrap .search_subttl {
 				font-size: 16px;
 			}
+
 			.SearchSect_wrap .search_subttl .small-txt {
 				font-size: 11px;
 			}
@@ -379,29 +386,37 @@ require_once($Root . "/office_search/html_lib.php");
 				padding: 5px 5px 5px 50px;
 				width: 100%;
 			}
+
+			.SearchBy .SearchBy_ttl span::before {
+				left: 4.5%;
+			}
+
 			.SearchSect-easy .search_cont .SearchBy_cont label {
 				padding: 0;
 				width: 50%;
 			}
+
 			.SearchSect-easy .search_cont .SearchBy_cont label:first-child {
 				width: 100%;
 			}
+
 			.SearchSect-easy .search_cont .SearchBy_cont .SearchBy_inputWrap {
 				display: block;
 				margin: 0 0 10px;
 				width: 280px;
 			}
+
 			.SearchSect-easy .search_cont .SearchBy_cont .SearchBy_buttonWrap {
 				display: block;
 				text-align: left;
 				width: 100%;
 			}
+
 			.SearchSect .SearchSect_wrap .SearchBtn {
 				margin-left: 0;
 			}
 
 		}
-
 	</style>
 	<!--[if lt IE 9]>
 	<script src="js/html5shiv.js"></script>
